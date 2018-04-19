8de38460cc8fe2804d568b323feebb3bf2d7c059
ZsnnsZ <925453683@qq.com>
2018-04-19 22:45:47 +0800

    Update getUrls.py

--- getUrls.py
+++ getUrls.py
@@ -10,30 +10,30 @@ allowSuffix = ['jpeg', 'png', 'gif']
 results = []
 
 def upload(args):
-        files = []
-        for arg in args:
-            file = open(arg, 'rb')
-            fileName = arg.split('/')[-1]
-            imgType = imghdr.what(file)
-            if imgType in allowSuffix:
-                files.append({'smfile': (fileName, file, 'image/jpeg')})
-        data = {
-            'ssl': True
-        }
-        with open('/Users/macbookair/Desktop/smmsTool/photoUrls.txt', 'at+') as f:
-            for file in files:
-                response = requests.post(uploadUrl, data=data, files=file)
-                json = response.json()
-                if json['code'] == 'success':
-                    # result = '''[%s](%s)''' % (json['data']['filename'], json['data']['url'])
-                    # f.write(result)
-                    result = '%s' % (json['data']['url']) + '\n'
-                    results.append(result)
-                    f.write(result)
-                else:
-                    results.append(response.json())
-
-        print(results)
+    files = []
+    for arg in args:
+        file = open(arg, 'rb')
+        fileName = arg.split('/')[-1]
+        imgType = imghdr.what(file)
+        if imgType in allowSuffix:
+            files.append({'smfile': (fileName, file, 'image/jpeg')})
+    data = {
+        'ssl': True
+    }
+    # edit the file path as you want
+    with open('/Users/macbookair/Desktop/smmsTool/photoUrls.txt', 'at+') as f:
+        for file in files:
+            response = requests.post(uploadUrl, data=data, files=file)
+            json = response.json()
+            if json['code'] == 'success':
+                # result = '''![%s](%s)''' % (json['data']['filename'], json['data']['url'])
+                # f.write(result)
+                result = '%s' % (json['data']['url']) + '\n'
+                results.append(result)
+                f.write(result)
+            else:
+                results.append(response.json())
+    print(results)
 
 def history():
     data = {
